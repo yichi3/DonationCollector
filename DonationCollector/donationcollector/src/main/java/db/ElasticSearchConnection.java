803874853c6e67d17730b80949f52038444b67d6
Sze Wai Celeste Yuen <szewaiyuen6@fb.com>
2020-07-13 23:43:44 -0700

    only files related to DB and .gitignore

--- DonationCollector/donationcollector/src/main/java/db/ElasticSearchConnection.java
+++ DonationCollector/donationcollector/src/main/java/db/ElasticSearchConnection.java
@@ -1,32 +1,64 @@
 package db;
 
 import org.apache.http.HttpHost;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.client.indices.CreateIndexRequest;
-import org.elasticsearch.client.indices.CreateIndexResponse;
-import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
+import org.json.JSONObject;
+
+import Entity.User;
 
 public class ElasticSearchConnection {
+	private RestHighLevelClient client;
 
-	public static void main(String[] args) {
-		RestHighLevelClient client = new RestHighLevelClient(
-				RestClient.builder(new HttpHost("35.225.69.232", 9200, "http")));
+	public void elasticSearchConnection() {
+		client = new RestHighLevelClient(RestClient.builder(new HttpHost(ElasticSearchDBUtil.INSTANCE,
+				ElasticSearchDBUtil.PORT_NUM, ElasticSearchDBUtil.CONNECTION_TYPE)));
+	}
 
-		CreateIndexRequest request = new CreateIndexRequest("test");
-
-		request.source(
-				"{\n" + "    \"mappings\" : {\n" + "        \"properties\" : {\n"
-						+ "            \"message\" : { \"type\" : \"text\" }\n" + "        }\n" + "    },\n"
-						+ "    \"aliases\" : {\n" + "        \"test_alias\" : {}\n" + "    }\n" + "}",
-				XContentType.JSON);
+	// Change this type to the actual user type once ready
+	public void addUser(User user) {
+		JSONObject userObj = user.toJSONObject();
+		XContentBuilder builder;
 		try {
-			CreateIndexResponse createIndexResponse = client.indices().create(request, RequestOptions.DEFAULT);
-
+			builder = XContentFactory.jsonBuilder();
+			builder.startObject();
+			{
+				builder.field("userId", userObj.getString("user_id"));
+				builder.field("userName", userObj.getString("name"));
+				builder.field("userType", userObj.getString("UserType"));
+				builder.field("email", userObj.getString("email"));
+				builder.field("address", userObj.getString("address"));
+				// to-fix hard-coded for now
+				builder.timeField("postDate", "2020-06-30");
+			}
+			builder.endObject();
+			IndexRequest indexRequest = new IndexRequest("users").id(userObj.getString("user_id")).source(builder);
+			IndexResponse response = client.index(indexRequest, RequestOptions.DEFAULT);
+			System.out.print(response);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
 	}
+
+	// placeholder
+//	public void addItem(MockItem item) {
+//
+//	}
+
+	// placeholder
+//	public void queryItemByLocation() {
+//
+//	}
+
+	// Placeholder
+//	public void queryItemByUserId() {
+//
+//	}	
 
 }
