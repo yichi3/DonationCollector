7292b0a12d4e41d0e98acfb18c1dc6384ce27812
Mingfu Zou <charles@Mingfus-MacBook-Pro.local>
2020-07-22 19:19:11 -0400

    0722

--- DonationCollector/donationcollector/src/main/java/rpc/SearchPostsNGO.java
+++ DonationCollector/donationcollector/src/main/java/rpc/SearchPostsNGO.java
@@ -4,21 +4,24 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Map;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import com.google.gson.Gson;
 
 import db.ElasticSearchConnection;
 
 /**
  * Servlet implementation class SearchPostsNGO
  */
-
 public class SearchPostsNGO extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
+       
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -26,25 +29,44 @@ public class SearchPostsNGO extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
-    
-    /**
-	 * @throws IOException 
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-    
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    	// assume the distance is a double for now, depending on request
-    	double distance = Double.parseDouble(request.getParameter("distance"));
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		double distance = Double.parseDouble(request.getParameter("distance"));
     	double lat = Double.parseDouble(request.getParameter("lat"));
 		double lng = Double.parseDouble(request.getParameter("lon")); 
 		ElasticSearchConnection connection = new ElasticSearchConnection();
 		connection.elasticSearchConnection();
 		
 		ArrayList<Map<String, Object>> hits = connection.queryItemByLocation(lat, lng, distance);
-        for (Map<String, Object> hit : hits) {
-        	
+        
+		try {
+			connection.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		JSONArray resultArray = new JSONArray();
+		for (Map<String, Object> hit : hits) {
+        	Gson gson = new Gson();
+        	String itemString = gson.toJson(hit);
+        	JSONObject obj = new JSONObject(itemString);
+        	resultArray.put(obj);
         }
-      
-        		
-    }
+		
+		RpcHelper.writeJsonArray(response, resultArray);
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
 }
