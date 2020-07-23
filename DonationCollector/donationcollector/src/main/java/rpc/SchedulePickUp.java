5c6dfe0c7335ea9553736816663b6abd1e6c8eaa
Wenyuan Huang <t.wenyuanhuang@gmail.com>
2020-07-23 17:06:14 -0400

    Consolidate v3

---
+++ DonationCollector/donationcollector/src/main/java/rpc/SchedulePickUp.java
@@ -0,0 +1,66 @@
+package rpc;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import db.ElasticSearchConnection;
+
+/**
+ * Servlet implementation class SchedulePickUp
+ */
+public class SchedulePickUp extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public SchedulePickUp() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String itemId = request.getParameter("itemId");
+		String ngoId = request.getParameter("ngoId");
+		String pickUpNGOName = request.getParameter("ngoName");
+		String availablePickUpTime = request.getParameter("pickUpDate");
+		
+
+		ElasticSearchConnection connection = new ElasticSearchConnection();
+		connection.elasticSearchConnection();
+		Map<String, Object> hit = connection.updateItemPickUpInfo(itemId, ngoId, pickUpNGOName, availablePickUpTime);
+		try {
+			connection.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		if (hit.isEmpty()) {
+			response.sendError(404, "cannot find this item");
+			return;
+		}
+		response.setStatus(204);
+	}
+
+}
+
+// map empty 
