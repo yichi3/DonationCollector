0669a90ee0f48895043d7802039e91841c87d2bf
Haiqing Wang <47518308+haiqingwang1005@users.noreply.github.com>
2020-07-24 19:39:18 -0700

    Update ConfirmPickUp.java

--- DonationCollector/donationcollector/src/main/java/rpc/ConfirmPickUp.java
+++ DonationCollector/donationcollector/src/main/java/rpc/ConfirmPickUp.java
@@ -34,8 +34,6 @@ public class ConfirmPickUp extends HttpServlet {
 		connection.elasticSearchConnection();
 		
 		Map<String, Object> hit = connection.markItemComplete(itemId, ngoId);
-		System.out.println("111");
-		System.out.println(hit);
 		try {
 			connection.close();
 		} catch (Exception e) {
